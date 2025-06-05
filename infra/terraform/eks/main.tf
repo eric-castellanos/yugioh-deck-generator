1534be7ac09e98a1fb696a01646f41f79a7baaa5
eric-castellanos <ericcastellanos229@gmail.com>
2025-06-05 10:11:19 -0400

    update tf files

--- infra/terraform/eks/main.tf
+++ infra/terraform/eks/main.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "20.8.4"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = var.cluster_version
   subnet_ids      = var.subnet_ids
   vpc_id          = var.vpc_id
 
