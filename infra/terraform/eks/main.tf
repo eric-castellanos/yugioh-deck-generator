6934aef0c2d449dd1524e488a3e0114467bab0ac
eric-castellanos <ericcastellanos229@gmail.com>
2025-06-11 15:40:36 -0400

    remove encryption

--- infra/terraform/eks/main.tf
+++ infra/terraform/eks/main.tf
@@ -1,25 +1,19 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.1.0"
+  version = "20.37.0"
 
-  cluster_name    = var.cluster_name
+  cluster_name = var.cluster_name
   cluster_version = var.cluster_version
-  subnet_ids      = var.subnet_ids
-  vpc_id          = var.vpc_id
+  subnet_ids = var.subnet_ids
+  vpc_id = var.vpc_id
 
-  enable_irsa     = true
-  create_kms_key = true
-  
-  encryption_config = {
-    resources = ["secrets"]
-  }
+  enable_irsa = true
 
   eks_managed_node_groups = {
     default_node_group = {
       desired_size = 2
       max_size     = 3
       min_size     = 1
-
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
     }
