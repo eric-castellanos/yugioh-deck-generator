89a1bae93beb9109f170dbee260599bf8363e37d
eric-castellanos <ericcastellanos229@gmail.com>
2025-06-13 17:09:35 -0400

    remote back end changes

--- infra/terraform/terraform.tf
+++ infra/terraform/terraform.tf
@@ -6,6 +6,14 @@ terraform {
     }
   }
   required_version = ">= 1.0.0"
+
+  backend "s3" {
+    bucket         = "yugioh-mlflow-terraform-state"
+    key            = "mlflow/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
