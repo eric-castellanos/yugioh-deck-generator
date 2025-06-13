fdb5996b0ad9a613376703958f6d595755bf9f9f
eric-castellanos <ericcastellanos229@gmail.com>
2025-06-13 14:48:02 -0400

    check for existing VPC before trying to create a new one

--- infra/terraform/vpc/main.tf
+++ infra/terraform/vpc/main.tf
@@ -1,27 +1,12 @@
-locals {
-  use_existing_vpc = var.vpc_id != null && var.vpc_id != ""
-  final_vpc_id     = local.use_existing_vpc ? var.vpc_id : module.vpc[0].vpc_id
-}
+data "aws_vpc" "existing" {
+  filter {
+    name   = "tag:Environment"
+    values = [var.environment]
+  }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.1"
-
-  count = local.use_existing_vpc ? 0 : 1
-
-  name = var.name
-  cidr = var.vpc_cidr
-
-  azs             = var.azs
-  public_subnets  = var.public_subnet_cidrs
-  private_subnets = var.private_subnet_cidrs
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Environment = var.environment
-    Project     = "mlflow"
+  filter {
+    name   = "tag:Project"
+    values = ["mlflow"]
   }
 }
 
@@ -46,6 +31,33 @@ data "aws_subnets" "private" {
   filter {
     name   = "vpc-id"
     values = [local.final_vpc_id]
+  }
+}
+
+locals {
+  use_existing_vpc = var.vpc_id != null && var.vpc_id != "" || length(data.aws_vpc.existing.id) > 0
+  final_vpc_id     = local.use_existing_vpc ? (var.vpc_id != null && var.vpc_id != "" ? var.vpc_id : data.aws_vpc.existing.id) : module.vpc[0].vpc_id
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  count = local.use_existing_vpc ? 0 : 1
+
+  name = var.name
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  public_subnets  = var.public_subnet_cidrs
+  private_subnets = var.private_subnet_cidrs
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Environment = var.environment
+    Project     = "mlflow"
   }
 }
 
