643f55da33c3887b9339f69b9001965b31669133
eric-castellanos <ericcastellanos229@gmail.com>
2025-08-05 15:34:50 -0400

    remove outer level files

--- src/deck_generation/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Simple Yu-Gi-Oh! Deck Generation Module
-
-A streamlined approach to generating Yu-Gi-Oh! decks from clustered card data.
-"""
-
-from .deck_generator import SimpleDeckGenerator, DeckMetadata, CardType
-
-__all__ = ['SimpleDeckGenerator', 'DeckMetadata', 'CardType']
